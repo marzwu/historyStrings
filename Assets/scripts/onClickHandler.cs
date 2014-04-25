fdfea6722bc9b21dcdf616980690493b458233a3
marzwu <marzwu@gmail.com>
2014-04-25 21:28:42 +0800

    添加点击事件脚本

---
+++ Assets/scripts/onClickHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class onClickHandler : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		void OnClick ()
+		{
+				Debug.Log ("hello world");
+				print ("print");
+		
+				GameObject label = GameObject.Find ("name0");
+				string [] names = {"go", "where", "here"};
+				int index = (int)(Random.Range((float)0.0, (float)2.9999));
+				label.GetComponent<UILabel> ().text = names [index];
+		}
+
+
+}
